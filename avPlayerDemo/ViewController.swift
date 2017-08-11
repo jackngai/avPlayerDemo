004e0be99fd1696dccaa4294ffd6f367c77e5624
Jack Ngai <jackngai@gmail.com>
2017-08-10 22:52:55 -0400

    Working demo

--- avPlayerDemo/ViewController.swift
+++ avPlayerDemo/ViewController.swift
@@ -7,8 +7,35 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
+    
+    var playerViewController = AVPlayerViewController()
+    var playerView = AVPlayer()
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let fileURL = Bundle.main.path(forResource: "1", ofType: "mp4")
+
+        playerView = AVPlayer(url: URL(fileURLWithPath: fileURL!))
+        
+        playerViewController.player = playerView
+        
+        present(playerViewController, animated: true)
+       
+        playerViewController.videoGravity = "AVLayerVideoGravityResizeAspectFill"
+        playerViewController.showsPlaybackControls = false
+        playerViewController.player?.play()
+        
+        playerViewController.showsPlaybackControls = true
+        
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
